691b8a3d7a0ca1c2289aa8ff74eb1ce9d32f3fd9
sh_wifi <6321058@ed.tus.ac.jp>
2024-11-12 11:33:09 +0900

    test

---
+++ Group-F-UnityProject/Assets/_Completed-Assets/Scripts/Managers/test.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample101
+{
+    class Program
+    {
+        /*
+         *  HelloWorld
+         */
+        static void Main(string[] args)
+        {
+            //  コンソールにHelloWorldと表示して終了
+            Console.WriteLine("HelloWorld.");
+        }
+    }
+}
