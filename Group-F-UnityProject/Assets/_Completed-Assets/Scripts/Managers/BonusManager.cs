fe1a8b8b31f5ae3ecab6c465c2c7cb19315cae20
tsuka2626 <yusuke1026rusi@gmail.com>
2024-12-10 00:11:16 +0900

    ボーナスで渡すアイテムの名前と個数を簡単に変更可能に

--- Group-F-UnityProject/Assets/_Completed-Assets/Scripts/Managers/BonusManager.cs
+++ Group-F-UnityProject/Assets/_Completed-Assets/Scripts/Managers/BonusManager.cs
@@ -8,35 +8,43 @@ public class BonusManager : MonoBehaviour
     public TMP_Text item2Text; // アイテム2のテキスト
     public GameObject mainMenuPage; // メインメニュー
 
-    void Start(){
+    [SerializeField] private string bonusMessage = "You received your login bonus!";
+    [SerializeField] private string bonusItem1Name = "Add HP Item";
+    [SerializeField] private int addNumItem1 = 1;
+    [SerializeField] private string bonusItem2Name = "Add Speed Item";
+    [SerializeField] private int addNumItem2 = 1;
+
+    void Start()
+    {
         AppState.CurrentPage = "Bonus";
-        Debug.Log($"Current Page: {AppState.CurrentPage}");
+
     }
+
     // ログインボーナス画面
     public void OnBonusPageShown()
     {
-        Debug.Log("OnBonusPageShown called.");
+
 
         string userName = PlayerPrefs.GetString("currentUser", "defaultUser");
         int item1 = PlayerPrefs.GetInt(userName + "_item1", 0);
         int item2 = PlayerPrefs.GetInt(userName + "_item2", 0);
 
-        Debug.Log($"Before bonus: Item1 = {item1}, Item2 = {item2}");
+
 
         // アイテム増加処理
-        PlayerPrefs.SetInt(userName + "_item1", item1 + 1);
-        PlayerPrefs.SetInt(userName + "_item2", item2 + 1);
+        PlayerPrefs.SetInt(userName + "_item1", item1 + addNumItem1);
+        PlayerPrefs.SetInt(userName + "_item2", item2 + addNumItem2);
         PlayerPrefs.Save();
 
-        Debug.Log($"After bonus: Item1 = {item1 + 1}, Item2 = {item2 + 1}");
+
 
         // アイテム数を表示
-        item1Text.text = "Get Item1! Add HP Item: " + (item1 + 1);
-        item2Text.text = "Get Item2! Add Speed Item: " + (item2 + 1);
+        item1Text.text = "Get " + bonusItem1Name + " : "+ addNumItem1;
+        item2Text.text = "Get " + bonusItem2Name + " : "+ addNumItem2;
 
         // ボーナスメッセージ
-        bonusText.text = "You received your login bonus!";
-        Debug.Log("Bonus message updated.");
+        bonusText.text = bonusMessage;
+
     }
 
     // メインメニューへ戻る
@@ -46,4 +54,3 @@ public class BonusManager : MonoBehaviour
         this.gameObject.SetActive(false);
     }
 }
-
